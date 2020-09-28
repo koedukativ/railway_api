a8eab44d68449ca49e78f35802b15b5c8dc49c50
Joschka Cesar <cesar@posteo.de>
2020-09-28 17:10:20 +0200

    Setup Folder structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+// Setup of Routes
+app.use('/stations', require('./routes/stationsRoute')); 
+app.use('/trains', require('./routes/trainsRoute'));
+
+// Declare Port 3000 as default
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Express Server started on port ${PORT}`));
