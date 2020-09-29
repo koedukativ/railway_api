cfd44126a9ca1f161f8db7e90a9753917d6c19fe
Joschka Cesar <cesar@posteo.de>
2020-09-29 16:55:10 +0200

    Add moveTrain Route

--- src/controllers/moveController.js
+++ src/controllers/moveController.js
@@ -1,12 +1,28 @@
 const dbConnection = require('../dbConfig');
 
 const UserController = {
-    moveTrain: (req, res, next) => {
+    getTrain: (req, res, next) => {
         const { id } = req.params;
         dbConnection
             .query(`SELECT * FROM "trains" WHERE  id = ${id}`)
             .then((data) => res.json(data.rows))
             .catch((e) => console.log(e));
+    },
+    // Additional error checking: 
+    // 1. Train does not exist or NaN
+    // 2. Station does not exist or NaN
+    
+    // Move a Train to a Station params: 'station'
+    moveTrain: (req, res, next) => {
+        const { id } = req.params;
+        const { station } = req.query;
+        dbConnection
+            .query(`UPDATE "trains" SET current_station = '${station}' WHERE id = ${id} RETURNING *`)
+            .then((data) => res.json(data.rows))
+            .catch((e) => {
+                res.status(500).send();
+                console.log(e)}
+                );
     }
 };
 
--- src/routes/moveRoute.js
+++ src/routes/moveRoute.js
@@ -6,6 +6,9 @@ const moveController = require('../controllers/moveController');
 router.get('/', (req, res) => res.send('Move Route'));
 
 // Select Train by ID (Test Purpose)
-router.get('/:id', moveController.moveTrain);
+router.get('/:id', moveController.getTrain);
+
+// Update Train Station by ID
+router.put('/:id', moveController.moveTrain);
 
 module.exports = router;
