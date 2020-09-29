7361b2878e31e746015eb6b7d8268d29c14c496d
Scirius <68830011+Scirius@users.noreply.github.com>
2020-09-29 14:57:07 +0200

    changed API to port 3001

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ app.use('/stations', require('./routes/stationsRoute'));
 app.use('/trains', require('./routes/trainsRoute'));
 
 // Declare Port 3000 as default
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Express Server started on port ${PORT}`));
