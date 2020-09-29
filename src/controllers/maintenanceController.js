f67349f7b8073138512f989dcba0132bc3ae0fdb
Scirius <68830011+Scirius@users.noreply.github.com>
2020-09-29 16:53:01 +0200

    added cors, first maintenance route

--- src/controllers/maintenanceController.js
+++ src/controllers/maintenanceController.js
@@ -5,9 +5,14 @@ const maintenanceController = {
     getAll: (req, res, next) => {
 
      dbConnection
-          .query(`SELECT * FROM "trains"`)
+          .query(`SELECT t.id, t.name, t.company, t.length, t.current_station, t.maintenance, 
+          s.name AS stationname 
+          FROM "trains" t
+          LEFT JOIN stations s
+          ON s.id = t.current_station
+          ORDER by t.name;`)
           .then((data) => {
-              console.log(JSON.stringify(data, null, 4));
+              //console.log(JSON.stringify(data, null, 4));
               return res.json(data.rows);
           })
           .catch((e) => console.log(e));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const router = express.Router();
 require('dotenv').config();
+
+/* CORS handling to be able to access endpoints from POSTMAN and others */
+app.use(cors({ origin: "*" }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Setup of Routes
 app.use('/stations', require('./routes/stationsRoute')); 
