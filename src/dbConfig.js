221ed4f1d272c4dc988714e76b9c064ab2a74fc4
Joschka Cesar <cesar@posteo.de>
2020-09-29 12:35:54 +0200

    Add dbConfig

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const dbConnection = new Pool({
-
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE_NAME,
+    password: process.env.DB_PASS,
+    port: 5432
 })
 
 module.exports = dbConnection;
